7f517a3ca176e10778e237a0e03ca1098e3d271c
Jaime Crispi <jcrispis56@gmail.com>
2019-07-07 11:18:53 -0400

    Comenzado segunda version del documento de Numpy

--- Numpy Nuevo/1. Intro.py
+++ Numpy Nuevo/1. Intro.py
@@ -11,4 +11,25 @@ print(a)  # [1 2 3]
 
 a = np.array(['a', 1, 2, 'b'])  # Está mal
 
+# Tipo de dato
 print(type(a))  # <class 'numpy.ndarray'>
+
+# Array de dos dimensiones
+a = np.array([[1, 2, 3], [1, 2, 3]])  # Array de dos dimensiones
+print(a)
+# [[1 2 3]
+#  [1 2 3]]
+
+print(a)
+# [[1 2 3]
+#  [1 2 3]]
+
+a.shape     # (2, 3)
+a.ndim      # 2
+a.size      # 6
+a.itemsize  # 8
+a.dtype     # int64
+
+# Calcular el tamaño en memoria de mi Array
+print(f'Tamaño de mi array: {a.itemsize * a.size} bytes')
+# Tamaño de mi array: 48 bytes
