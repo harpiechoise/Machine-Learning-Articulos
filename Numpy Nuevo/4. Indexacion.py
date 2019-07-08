17426f250cf099626f649475fda4f879470221e0
Jaime Crispi <jcrispis56@gmail.com>
2019-07-08 17:03:43 -0400

    --- Auto-Git Commit ---

--- Numpy Nuevo/4. Indexacion.py
+++ Numpy Nuevo/4. Indexacion.py
@@ -3,6 +3,9 @@ import numpy as np  # Importamos numpy bajo el seudonimo
 a = np.arange(10) + 50  # Creamos un rango y le sumamos 50
 print(a[2])  # Si quiero acceder al tercer elemento
 
+print(a[-2]) # Si quiero el penultimo valor de mi Array
+# 58
+
 # Seleccionar un rango de elementos
 print(a[2:5])   # Seleccionar desde tercer elemento al sexto elemento
 # [52 53 54]
@@ -21,3 +24,41 @@ print(b)
 # [[ 5  5  5  5  5]
 #  [ 5  6  7  8  9]
 #  [ 5  7  9 11 13]]
+print(b[1][3])  # Acceder al tercer elemento del segundo array
+# 8
+# Metodo Optimo
+print(b[1, 3])  # Acceder al tercer elemento del segundo array
+# 8
+
+print(b[:5, 1])  # Cada valor de la columas de b
+# [5 6 7]
+
+# Version Optima
+print(b[:, 1])
+# [5 6 7]
+
+print(b[1, ])  # Filas
+# [5 6 7 8 9]
+
+print(b[-1, ])  # Ultima fila
+
+# Recorrer todos los valores de una Array multidimensional
+for i in a.flat:
+    print(i)
+# 50
+# 51
+# 52
+# 53
+# 54
+# 55
+# 56
+# 57
+# 58
+# 59
+
+# Filtrar arrays
+print(a[a > 55])  # Valores mayores a 55
+# [56 57 58 59]
+
+print(a[(a > 55) | (a == 50)])  # Operaciones de bits
+# [50 56 57 58 59]
