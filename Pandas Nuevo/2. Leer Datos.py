ff8d3a9719c6f659ce7a35caf21a79c9a33958d1
Jaime Crispi <jcrispis56@gmail.com>
2019-07-09 00:35:18 -0400

    --- Auto-Git Commit ---

--- Pandas Nuevo/2. Leer Datos.py
+++ Pandas Nuevo/2. Leer Datos.py
@@ -2,20 +2,15 @@ import pandas as pd  # Importamos pandas bajo el seudonimo
 
 # Leer datos en CSV
 df = pd.read_csv('iris.csv')  # Abrir un CSV con headers
-df.head()  # Primeros 5 valores del dataset
+df.head(2)  # Primeros 2 valores del dataset
 #    sepal length in cm   sepal width in cm ...
 # 0                 5.1                 3.5
 # 1                 4.9                 3.0
-# 2                 4.7                 3.2
-# 3                 4.6                 3.1
-# 4                 5.0                 3.6
-df.tail()  # Ultimos 5 valores del dataset
+
+df.tail(2)  # Ultimos 5 valores del dataset
 #      sepal length in cm   sepal width in cm ...
 # 145                 6.7                 3.0
 # 146                 6.3                 2.5
-# 147                 6.5                 3.0
-# 148                 6.2                 3.4
-# 149                 5.9                 3.0
 
 df = pd.read_csv('iris', encoding='utf-8')  # Leer un dataframe con encoding
 
@@ -27,3 +22,25 @@ print(df)
 # ..                  ...                 ...                  ...
 # 148                 6.2                 3.4                  5.4
 # 149                 5.9                 3.0                  5.1
+
+print(len(df))  # Cantidad de filas
+# 150
+
+# Leer un JSON
+df = pd.read_csv("iris1.csv", header=None)
+print(df)
+#        0    1    2    3               4
+# 0    5.1  3.5  1.4  0.2     Iris-setosa
+# 1    4.9  3.0  1.4  0.2     Iris-setosa
+# ..   ...  ...  ...  ...             ...
+# 148  6.2  3.4  5.4  2.3  Iris-virginica
+# 149  5.9  3.0  5.1  1.8  Iris-virginica
+
+# Webscraping
+# pip install lxml
+url = pd.read_html('https://www.rexegg.com/regex-quickstart.html')
+df = url[0]
+df.head(2)
+#   Character                                           Legend    Example  \
+# 0        \d               Most engines: one digitfrom 0 to 9  file_\d\d
+# 1        \d  .NET, Python 3: one Unicode digit in any script  file_\d\d
