76e816e59dfadc1e589c5864488a504f4bf0d607
Jaime Crispi <jcrispis56@gmail.com>
2019-07-10 16:31:32 -0400

    Terminado Merge Basico

--- Pandas Nuevo/4. Merging.py
+++ Pandas Nuevo/4. Merging.py
@@ -9,3 +9,21 @@ b = pd.DataFrame({'Peso': [600, 120],
 
 # Concatenar en filas
 c = pd.concat([a, b], axis=0)  # Concatenar en el eje de las filas
+print(c)
+# Ignorar indice
+c = pd.concat([a, b], axis=0, ignore_index=True)
+print(c)
+# Concatenacion en columnas
+c = pd.concat([a, b], axis=1)
+print(c)
+
+# Merging
+a = pd.DataFrame({'Ciudad': ['Santiago', 'La serena', 'Temuco'],
+                   'Viento KM': [30, 60, 20]})
+
+b = pd.DataFrame({'Ciudad': ['Santiago', 'La serena', 'Temuco'],
+                  'Humedad': [60, 20, 30], 
+                  'Direcion Viento': ['E', 'S', 'O']})
+
+c = pd.merge(a, b, on='Ciudad')
+print(c)
